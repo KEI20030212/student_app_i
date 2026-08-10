965ac172bdbbbed950afe1a9b554b227286023b7
KEI20030212 <tkey4392@gmail.com>
2026-08-11 01:51:42 +0900

    Add files via upload

--- views/word_quiz_maker.py
+++ views/word_quiz_maker.py
@@ -36,7 +36,7 @@ def render_word_quiz_maker_page():
 
     # --- メイン設定 ---
     # 🌟 修正1：選択肢を「キクタン」の4つに限定
-    target_options = ["キクタン8問", "キクタン16問", "キクタン32問", "キクタン50問", "キクタン8問(東)", "キクタン16問(東)", "キクタン32問(東)", "キクタン50問(東)", "WordCup20問", "WordCup100問", "WordCup200問", "英単語ターゲット1900(20問)", "英単語ターゲット1900(100問)", "英単語ターゲット1900(200問)", "iワーク中3英単語(東書)"]
+    target_options = ["キクタン8問", "キクタン16問", "キクタン32問", "キクタン50問", "キクタン8問(東)", "キクタン16問(東)", "キクタン32問(東)", "キクタン50問(東)", "WordCup20問", "WordCup100問", "WordCup200問", "英単語ターゲット1900(20問)", "英単語ターゲット1900(100問)", "英単語ターゲット1900(200問)", "iワーク中3英単語(東書)", "一問一答(理科)", "一問一答(社会)"]
     
     # 登録されている中から、対象の4つだけを表示（登録がない場合は警告）
     available_options = [opt for opt in target_options if opt in quiz_dict]
@@ -77,6 +77,10 @@ def render_word_quiz_maker_page():
         elif quiz_name == "英単語ターゲット1900(200問)":
             ranges, p_size = ["A1:AB27", "A29:AB55", "AC1:BD27", "AC29:BD55"], "A3"
             portrait_val = "false" 
+        elif quiz_name == "一問一答(理科)":
+            ranges, p_size = ["A1:I23", "J1:R23"], "B5"
+        elif quiz_name == "一問一答(社会)":
+            ranges, p_size = ["A1:I23", "J1:R23"], "B5"
         elif quiz_name == "キクタン8問(東)":
             ranges, p_size = ["A1:I18", "J1:R18"], "B5" 
         elif quiz_name == "キクタン16問(東)":
