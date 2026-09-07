17815980941870befdd06e9a53e6e312c0ba3c94
KEI20030212 <tkey4392@gmail.com>
2026-09-07 22:27:37 +0900

    Add files via upload

--- views/line_report.py
+++ views/line_report.py
@@ -2,7 +2,6 @@ import streamlit as st
 from views.line_report_generate import render_report_generation_tab
 from views.line_report_reply import render_parent_reply_tab
 from views.line_parent_reply_dashboard import render_parent_reply_dashboard
-from views.line_monthly_report import render_monthly_visual_report_tab
 from views.email_report_generate import render_email_report_tab
 
 def render_line_report_page():
@@ -25,12 +24,11 @@ def render_line_report_page():
 
     if can_use_reply:
         # 🌟 修正：タブを「4つ」に増やしました！
-        main_tab1, main_tab2, main_tab3, main_tab4, main_tab5 = st.tabs([
+        main_tab1, main_tab2, main_tab3, main_tab4 = st.tabs([
             "📱 LINEレポート一括生成",
             "📧 メール送信用レポート生成",
             "💬 保護者返信・ファン化度記録", 
             "📂 過去の返信アーカイブ",
-            "🖼️ 月末ビジュアルレポート生成" # 👈 これが新しい機能です！
         ])
         
         with main_tab1:
@@ -41,7 +39,5 @@ def render_line_report_page():
             render_parent_reply_tab()
         with main_tab4:
             render_parent_reply_dashboard()
-        with main_tab5:
-            render_monthly_visual_report_tab()
     else:
         render_report_generation_tab(can_use_report)
